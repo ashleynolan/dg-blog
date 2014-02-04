99d2548615379760ea913f85f6f321b528afa0b7
Ashley Nolan <nolly00@gmail.com>
2014-02-04 12:22:40 +0000

    Adding new blog post - heavy weight websites

--- dist/assets/js/helpers/setExternalLinks.js
+++ dist/assets/js/helpers/setExternalLinks.js
@@ -1,5 +1,8 @@
 /* used to get rid of _blank so your code validates w3c stylee */
 function setExternalLinks() {
+	var $postLinks = $('.post a');
+	$postLinks.attr('rel', 'external');
+
   var el_list = document.getElementsByTagName('A');
   for (i=0; i<el_list.length; i++) {
     if (el_list[i].getAttribute('rel') == 'external') {
